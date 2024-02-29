90e6bac63f583ab0a0788ea1a3ba49d643497c60
Tomislav <tomislavtroha69@gmail.com>
2024-02-29 20:07:10 +0100

    Upgrading Nuget packages;

--- AttendanceApp_WebAPI/WebAPI/SwimmingAppWebAPI/SwimmingApp.DAL/Utils/TokenGenerator.cs
+++ AttendanceApp_WebAPI/WebAPI/SwimmingAppWebAPI/SwimmingApp.DAL/Utils/TokenGenerator.cs
@@ -15,25 +15,25 @@ namespace SwimmingApp.BL.Utils
     {
         public static string CreateToken(UserModel user, IConfiguration configuration)
         {
-            List<Claim> claims = new List<Claim>
+                List<Claim> claims = new List<Claim>
             {
                 new Claim("FirstLastName", user.Name + " " + user.Surname),
                 new Claim("UserRoleId", user.UserRoleModel.RoleId.ToString()),
                 new Claim("UserID", user.UserId.ToString())
             };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("AppSettings:Token").Value));
-            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("AppSettings:Token").Value));
+                var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-            var token = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.Now.AddHours(2),
-                signingCredentials: cred
-                );
+                var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddHours(2),
+                    signingCredentials: cred
+                    );
 
-            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+                var jwt = new JwtSecurityTokenHandler().WriteToken(token);
 
-            return jwt;
+                return jwt;
+            }
         }
-    }
 }
